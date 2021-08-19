eb03d21f556132b64e4a403b7b9636e5c7648dcd
soos <62181284+sooswastaken@users.noreply.github.com>
2021-08-19 19:59:47 -0400

    Version 2.0

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,8 +4,12 @@ menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
+    document.getElementsByClassName("mobile-nav")[0].style.display = "initial";
+
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
+    document.getElementsByClassName("mobile-nav")[0].style.display = "none";
+
   }
 });
