839b954f8b420453a74832f02ef8a7b05dd425d8
soos <62181284+sooswastaken@users.noreply.github.com>
2021-08-20 08:04:59 -0400

    Version 3.0

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,15 +1,38 @@
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
+var id = null;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
-    document.getElementsByClassName("mobile-nav")[0].style.display = "initial";
+    var elem = document.getElementsByClassName("mobile-nav")[0]
+    var pos = -30;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 0) {
+        clearInterval(id);
+      } else {
+        pos = pos + 0.5; 
+        elem.style.right = pos + 'em'; 
+      }
+    }
 
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
-    document.getElementsByClassName("mobile-nav")[0].style.display = "none";
+    var elem = document.getElementsByClassName("mobile-nav")[0]
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+      if (pos == -30) {
+        clearInterval(id);
+      } else {
+        pos = pos - 0.5; 
+        elem.style.right = pos + 'em'; 
+      }
+    }
 
   }
 });
